24b7b09f55e4de25bb28491f58dd35cd60e00b8f
lucaspalomodevelop <lucas.palomo@t-online.de>
2022-11-17 17:51:44 +0100

    new folderstructure

--- dashboard/src/pages/Dashboard/index.js
+++ dashboard/src/pages/Dashboard/index.js
@@ -1,11 +1,11 @@
 import { Stack, Card, Alert } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Dashboard.css";
-import JssteStatus from "./JssteStatus";
-import WebsrvStatus from "./websrvStatus";
-import AppStatus from "./appStatus";
-import ConfigStatus from "./configStatus";
-import LogStatus from "./logStatus";
+import JssteStatus from "../../components/JssteStatus";
+import WebsrvStatus from "../../components/websrvStatus";
+import AppStatus from "../../components/appStatus";
+import ConfigStatus from "../../components/configStatus";
+import LogStatus from "../../components/logStatus";
 
 function Dashboard() {
   return (
